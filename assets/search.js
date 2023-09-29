437ebd0a5ef3d4b07ae2a1e3dd9e82776211aaa2
Endeyr <aaron.thompson42594@gmail.com>
2023-09-29 12:58:24 -0400

    fixed name

--- assets/search.js
+++ assets/search.js
@@ -1,5 +1,5 @@
-import pokemon from 'pokemontcgsdk'
-pokemon.configure({ 'api.Key': '' })
+import pokemon from 'pokemontcgsdk';
+pokemon.configure({ 'api.Key': 'bd895527-41d0-417e-a109-3667e7b8dddb' })
 
 const initApp = () => {
 	const searchBtn = document.getElementById('search-btn')
