249dc3dae57e81cdeb4dfde0decb553f6d0ff056
Endeyr <aaron.thompson42594@gmail.com>
2023-09-29 15:46:02 -0400

    add contact to admin and fix user search ui if no collections

--- core/admin/__init__.py
+++ core/admin/__init__.py
@@ -1,9 +1,11 @@
 from core.admin.profile import ProfileAdmin
 from core.admin.collection import CollectionAdmin
 from core.admin.card import CardAdmin
+from core.admin.contact import ContactAdmin
 
 __all__ = [
     ProfileAdmin,
     CollectionAdmin,
     CardAdmin,
+    ContactAdmin,
 ]
