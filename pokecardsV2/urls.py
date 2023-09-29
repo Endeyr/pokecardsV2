bd7e04549cc09063bdf4032744575dd23d91a005
Endeyr <aaron.thompson42594@gmail.com>
2023-09-29 14:57:02 -0400

    updated for production

--- pokecardsV2/urls.py
+++ pokecardsV2/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls", namespace="core")),
     path("user/", include("users.urls", namespace="users")),
-    path("__debug__/", include("debug_toolbar.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
@@ -16,3 +15,4 @@ if not settings.PRODUCTION:
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += path("__debug__/", include("debug_toolbar.urls"))
