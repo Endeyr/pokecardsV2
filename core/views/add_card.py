59b126fdbae9590993111ba1c682c37729c72c6d
Endeyr <aaron.thompson42594@gmail.com>
2023-10-02 15:46:23 -0400

    fixed case of none

--- core/views/add_card.py
+++ core/views/add_card.py
@@ -20,29 +20,42 @@ def add_card(request):
                 card_obj.name = card_api.name
                 card_obj.number = card_api.number
 
-                if card_api.tcgplayer is None:
-                    card_obj.price = 0.00
-                elif card_api.tcgplayer.prices is None:
-                    card_obj.price = 0.00
-                elif card_api.tcgplayer.prices.normal:
-                    card_obj.price = card_api.tcgplayer.prices.normal.market
-                elif card_api.tcgplayer.prices.holofoil:
-                    card_obj.price = card_api.tcgplayer.prices.holofoil.market
-                elif card_api.tcgplayer.prices.reverseHolofoil:
-                    card_obj.price = card_api.tcgplayer.prices.reverseHolofoil.market
-                elif card_api.tcgplayer.prices.unlimited:
-                    card_obj.price = card_api.tcgplayer.prices.unlimited.market
-                elif card_api.tcgplayer.prices.unlimitedHolofoil:
-                    card_obj.price = card_api.tcgplayer.prices.unlimitedHolofoil.market
-                else:
+                try:
+                    if card_api.tcgplayer is None:
+                        card_obj.price = 0.00
+                    elif card_api.tcgplayer.prices is None:
+                        card_obj.price = 0.00
+                    elif card_api.tcgplayer.prices.normal:
+                        card_obj.price = card_api.tcgplayer.prices.normal.market
+                    elif card_api.tcgplayer.prices.holofoil:
+                        card_obj.price = card_api.tcgplayer.prices.holofoil.market
+                    elif card_api.tcgplayer.prices.reverseHolofoil:
+                        card_obj.price = (
+                            card_api.tcgplayer.prices.reverseHolofoil.market
+                        )
+                    elif card_api.tcgplayer.prices.unlimited:
+                        card_obj.price = card_api.tcgplayer.prices.unlimited.market
+                    elif card_api.tcgplayer.prices.unlimitedHolofoil:
+                        card_obj.price = (
+                            card_api.tcgplayer.prices.unlimitedHolofoil.market
+                        )
+                    else:
+                        card_obj.price = 0.00
+                except AttributeError:
                     card_obj.price = 0.00
 
                 card_obj.rarity = card_api.rarity
                 card_obj.set = card_api.set.name
                 card_obj.small_image = card_api.images.small
-                card_obj.subtypes = ", ".join(card_api.subtypes)
+                if card_obj.subtypes is None:
+                    card_obj.subtypes = card_api.subtypes
+                else:
+                    card_obj.subtypes = ", ".join(card_api.subtypes)
                 card_obj.supertype = card_api.supertype
-                card_obj.types = ", ".join(card_api.types)
+                if card_obj.types is None:
+                    card_obj.types = card_api.types
+                else:
+                    card_obj.types = ", ".join(card_api.types)
                 card_obj.save()
                 collection.cards.add(card_obj)
         return redirect("core:collection", slug=collection.slug)
